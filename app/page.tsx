e2d272e51640fb87c4b0e21b7bf7b60033074dfb
KaberaSamuel <kaberanshutis@gmail.com>
2025-12-03 11:23:53 +0200

    setup MUI in the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import HeroSection from "@/components/Hero";
+
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen w-full">
-      <h1 className="text-teal-500">Welcome to TradeTalent</h1>
-      <p>Your gateway to top trade professionals.</p>
-
-      <p>You're seeing this now</p>
+    <div>
+      <HeroSection />
     </div>
   );
 }
